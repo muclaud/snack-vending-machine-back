3be2b0ed71bc3a0736450050d7be828a1753b94e
muclaud <ttunka08@gmail.com>
2021-05-25 23:07:41 +0300

    config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  SECRET_KEY: 'Kalush city',
+  MONGODB:
+    'mongodb+srv://user1:EOp6tBWBVM7MMOal@biathloncluster.g8i5u.mongodb.net/product-app?retryWrites=true&w=majority',
+};
